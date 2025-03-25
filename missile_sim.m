112a0dfedaf6bad46aa088f0bf192c845f36f5db
notjWm <jacksonm05@vt.edu>
2025-03-25 01:34:20 -0400

    Add files via upload

---
+++ missile_sim.m
@@ -0,0 +1,21 @@
+clc; clear all; close all;
+
+d2r = pi / 180;
+
+% Target Parameters
+v_t = 1000;
+x_t0 = 100;
+z_t0 = 20000;
+l_t0 = -2.5 * d2r;
+
+% Missile Parameters
+x_m0 = 2000;
+z_m0 = 0;
+l_m0 = 40 * d2r;
+
+% Gains
+kd = 1.1;
+ka = 4.5;
+ki = 14.3;
+kr = -0.37;
+
